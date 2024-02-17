cc06f9d3a43634008e38217a0c91262ec3c310d5
BenAnderson8181 <146896796+BenAnderson8181@users.noreply.github.com>
2024-02-17 15:02:49 -0700

    Removed customer content from header and some route pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,6 @@ const Header: NextPage = () => {
                 <Link href="/" className="text-2xl font-thin" passHref>Go Florida Charter</Link>
             </div>
             <div className="flex justify-evenly w-1/2 translate-y-2">
-                <Link href="/rentals" passHref>Charter Bus Rentals</Link>
-                <Link href="/shuttles" passHref>Shuttles</Link>
                 <div className="relative block cursor-pointer" onMouseLeave={() => setOpen(false)} onMouseEnter={() => setOpen(true)}>
                     Company&nbsp;<RiArrowDownSLine size={20} className="text-slate-100 inline" />
                     { 
@@ -30,9 +28,8 @@ const Header: NextPage = () => {
                 <Link href="/operators" passHref>Operators</Link>
             </div>
             <div className="flex justify-end">
-                    <Link href="/get-quote" className="bg-slate-100 rounded-full px-4 py-2 text-black font-semibold mr-6 hover:bg-slate-300">Get&nbsp;a&nbsp;Quote</Link>
-                    <Link href="/login" className="bg-black rounded-full px-6 py-2 text-slate-100 font-semibold border-2 border-slate-100 hover:border-slate-400">Login</Link>
-                </div>
+                <Link href="/login" className="bg-black rounded-full px-6 py-2 text-slate-100 font-semibold border-2 border-slate-100 hover:border-slate-400">Login</Link>
+            </div>
             <div className="flex items-center">
                 <UserButton />
             </div>
